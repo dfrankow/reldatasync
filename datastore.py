dc9c3d7482f149b9d0c518bb7d06e0026d08dd7f
Dan Frankowski <dfrankow@gmail.com>
2019-10-10 13:56:26 -0500

    Add PostgresDatastore

--- datastore.py
+++ datastore.py
@@ -1,7 +1,11 @@
 """An abstraction of a datastore, to use for syncing."""
 
+from abc import ABC, abstractmethod
+
 import logging
 from typing import Sequence, TypeVar, Generic
+
+import psycopg2
 
 ID = TypeVar('ID')
 
@@ -17,6 +21,22 @@ class Document(dict):
         super(Document, self).__init__(*arg, **kw)
         assert '_id' in self
 
+    @staticmethod
+    def _compare_vals(one, two):
+        # comparisons have to happen in the right order to respect None
+        if one is None and two is None:
+            return 0
+        elif one is None and two is not None:
+            return -1
+        elif one is not None and two is None:
+            return 1
+        elif one < two:
+            return -1
+        elif one > two:
+            return 1
+        else:
+            return 0
+
     def _compare(self, other):
         """Return -1 if doc1 < doc2, 0 if equal, 1 if doc1 > doc2"""
         # compare keys
@@ -29,17 +49,16 @@ class Document(dict):
             keys1 = sorted(self.keys())
             keys2 = sorted(other.keys())
             for idx in range(len(keys1)):
-                if keys1[idx] < keys2[idx]:
-                    return -1
-                elif keys1[idx] > keys2[idx]:
-                    return 1
+                keycmp = Document._compare_vals(keys1[idx], keys2[idx])
+                if keycmp != 0:
+                    return keycmp
 
             # keys were all the same, now compare values
             for idx in range(len(self)):
-                if self[keys1[idx]] < other[keys2[idx]]:
-                    return -1
-                elif self[keys1[idx]] > other[keys2[idx]]:
-                    return 1
+                valcmp = Document._compare_vals(
+                    self[keys1[idx]], other[keys2[idx]])
+                if valcmp != 0:
+                    return valcmp
 
             # everything was equal
             return 0
@@ -66,39 +85,34 @@ class Document(dict):
         return Document(super().copy())
 
 
-class MemoryDatastore(Generic[ID]):
-    """The memory datastore is transient.  It's only useful for testing."""
+class Datastore(Generic[ID], ABC):
     def __init__(self, datastore_id: str):
         self.id = datastore_id
-        self.datastore = {}
         self.sequence_id = 0
         self.peer_seq_ids = {}
 
-    def get(self, docid: ID) -> Document:
-        """Return doc, or None if not present."""
-        doc = self.datastore.get(docid, None)
-        # Return a copy so our internals cannot be modified
-        if doc:
-            doc = doc.copy()
-        return doc
+    def __enter__(self):
+        pass
 
-    def put(self, doc: Document) -> None:
-        """Put doc under docid.
+    def __exit__(self, *args):
+        pass
 
-        If no seq, give it one.
-        """
+    def _increment_sequence_id(self):
+        self.sequence_id += 1
+
+    def _pre_put(self, doc):
         # copy doc so we don't modify caller's doc
         doc = doc.copy()
 
         if doc.get(_REV, None) is None:
-            self.sequence_id += 1
+            self._increment_sequence_id()
             doc[_REV] = self.sequence_id
 
-        docid = doc[_ID]
-        self.datastore[docid] = doc
         logger.debug("datastore %s put docid %s seq %s doc %s" % (
-            self.id, docid, doc[_REV], doc
+            self.id, doc[_ID], doc[_REV], doc
         ))
+
+        return doc
 
     def put_if_needed(self, doc: Document) -> None:
         """Put doc under docid if seq is greater"""
@@ -113,28 +127,18 @@ class MemoryDatastore(Generic[ID]):
                          "(compared to doc %s seq %s)" % (
                           docid, doc, seq, my_doc, my_seq))
 
-    def get_docs_since(self, the_seq: int) -> Sequence[Document]:
-        """Get docs put with seq > the_seq
-        """
-        result = []
-        for docid, doc in self.datastore.items():
-            if doc[_REV] > the_seq:
-                result.append(doc)
-        return result
-
     def delete(self, docid: ID) -> None:
         """Delete an doc in the datastore.
 
         Returns silently if the doc is not in the datastore.
         """
-        doc = self.datastore.get(docid, None)
+        doc = self.get(docid)
         if doc and not doc.get(_DELETED, False):
-            # The doc is copied in put, so we can modify it how we wish
             doc[_DELETED] = True
-
             # Deletion makes a new rev
-            self.sequence_id += 1
+            self._increment_sequence_id()
             doc[_REV] = self.sequence_id
+            self.put(doc)
             logger.debug("deleted doc: %s" % doc)
 
     def get_peer_sequence_id(self, peer: str):
@@ -146,3 +150,148 @@ class MemoryDatastore(Generic[ID]):
         assert seq >= self.get_peer_sequence_id(peer), (
             'seq %s peer seq %s' % (seq, self.get_peer_sequence_id(peer)))
         self.peer_seq_ids[peer] = seq
+
+    @abstractmethod
+    def get(self, docid: ID) -> Document:
+        pass
+
+    @abstractmethod
+    def put(self, doc: Document) -> None:
+        pass
+
+    @abstractmethod
+    def get_docs_since(self, the_seq: int) -> Sequence[Document]:
+        pass
+
+
+class MemoryDatastore(Datastore):
+    """An in-memory transient datastore, only useful for testing."""
+    def __init__(self, datastore_id: str):
+        super().__init__(datastore_id)
+        self.datastore = {}
+
+    def get(self, docid: ID) -> Document:
+        """Return doc, or None if not present."""
+        doc = self.datastore.get(docid, None)
+        # Return a copy so our internals cannot be modified
+        if doc:
+            doc = doc.copy()
+        return doc
+
+    def put(self, doc: Document) -> None:
+        """Put doc under docid.
+
+        If no seq, give it one.
+        """
+        doc = self._pre_put(doc)
+        self.datastore[doc[_ID]] = doc
+
+    def get_docs_since(self, the_seq: int) -> Sequence[Document]:
+        """Get docs put with seq > the_seq, unordered."""
+        for docid, doc in self.datastore.items():
+            if doc[_REV] > the_seq:
+                yield doc
+
+
+class PostgresDatastore(Datastore):
+    def __init__(self, datastore_id: str, conn_str: str,
+                 tablename: str):
+        super().__init__(datastore_id)
+        self.tablename = tablename
+        self.conn_str = conn_str
+
+    def __enter__(self):
+        super().__enter__()
+        self.conn = psycopg2.connect(self.conn_str)
+        self.cursor = self.conn.cursor()
+
+        # Check that the right tables exist
+        self.cursor.execute("SELECT sequence_id FROM data_sync_revisions")
+        self.sequence_id = self.cursor.fetchone()[0]
+
+        # Get the column names for self.tablename
+        self.cursor.execute("SELECT * FROM %s LIMIT 0" % self.tablename)
+        self.columnnames = [desc[0] for desc in self.cursor.description]
+        self.nonid_columnnames = [name for name in self.columnnames
+                                  if name != _ID]
+
+        # Check that self.tablename has _id, _deleted, and _rev
+        for field in (_ID, _REV, _DELETED):
+            if field not in self.columnnames:
+                raise NameError("Field '%s' not in table '%s'" % (
+                    field, self.tablename))
+
+        # TODO(dan): Check self.tablename has a unique index on _id
+        # Required for proper functioning of Postgres UPSERT
+        # See also https://stackoverflow.com/a/36799500/34935
+
+        return self
+
+    def __exit__(self, *args):
+        super().__exit__()
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+
+    def _row_to_doc(self, docrow):
+        the_dict = {}
+        assert len(docrow) == len(self.columnnames)
+        for idx in range(len(docrow)):
+            the_dict[self.columnnames[idx]] = docrow[idx]
+        # Treat '_deleted' specially: get rid of it unless True
+        if not the_dict[_DELETED]:
+            del the_dict[_DELETED]
+        return Document(the_dict)
+
+    def get(self, docid: ID) -> Document:
+        """Return doc, or None if not present."""
+        doc = None
+        self.cursor.execute(
+            "SELECT * FROM %s WHERE _id=%%s" % self.tablename, docid)
+        docrow = self.cursor.fetchone()
+        if docrow:
+            doc = self._row_to_doc(docrow)
+            # assert there was only one result
+            assert self.cursor.fetchone() is None, 'docid was %s' % docid
+        return doc
+
+    def put(self, doc: Document) -> None:
+        """Put doc under docid.
+
+        If no seq, give it one.
+        """
+        doc = self._pre_put(doc)
+
+        # "ON CONFLICT" requires postgres 9.5+
+        set_statement = ', '.join("%s=EXCLUDED.%s " % (col, col)
+                                  for col in self.columnnames)
+        upsert_statement = (
+            "INSERT INTO %s (%s) VALUES (%s) ON CONFLICT (_id) DO UPDATE"
+            " SET %s" % (
+                self.tablename,
+                ','.join(self.columnnames),
+                ','.join([r'%s' for _ in self.columnnames]),
+                set_statement))
+
+        self.cursor.execute(
+            upsert_statement,
+            tuple([doc.get(key, None) for key in self.columnnames]))
+
+    def get_docs_since(self, the_seq: int) -> Sequence[Document]:
+        """Get docs put with seq > the_seq, unordered."""
+        # TODO(dan): Use this safer way if I can get it to work:
+        # self.cursor.execute(
+        #     "SELECT * FROM %s WHERE _rev > %%s" % self.tablename, the_seq)
+        self.cursor.execute(
+            "SELECT * FROM %s WHERE _rev > %s" % (self.tablename, the_seq))
+        for docrow in self.cursor.fetchall():
+            yield self._row_to_doc(docrow)
+
+    def _increment_sequence_id(self):
+        self.cursor.execute(
+            "UPDATE data_sync_revisions set sequence_id = sequence_id+1"
+            " RETURNING sequence_id")
+        new_val = self.cursor.fetchone()[0]
+        super()._increment_sequence_id()
+        assert self.sequence_id == new_val
--- sync_tests.py
+++ sync_tests.py
@@ -1,67 +1,70 @@
+import logging
+import psycopg2
 import unittest
 import random
 
-from datastore import MemoryDatastore, Document
+from datastore import MemoryDatastore, PostgresDatastore, Document
 from sync import sync_both
 
+logger = logging.getLogger(__name__)
 
-class TestDatastore(unittest.TestCase):
+
+# nosetests ignores tests starting with underscore
+# See also https://stackoverflow.com/a/50380006/34935
+class _TestDatastore(unittest.TestCase):
+    """Base class for testing datastores."""
+
+    def setUp(self):
+        self.server = None
+        self.client = None
+
     def test_sync1(self):
         """Non-overlapping documents from datastore"""
-        server = MemoryDatastore('server')
-        client = MemoryDatastore('client')
-
         # server makes object A v1
-        server.put(Document({'_id': 'A', 'value': 'val1'}))
+        self.server.put(Document({'_id': 'A', 'value': 'val1'}))
         # client makes object B v1
-        client.put(Document({'_id': 'B', 'value': 'val2'}))
+        self.client.put(Document({'_id': 'B', 'value': 'val2'}))
 
         # sync leaves both server and client with A val1, B val2
-        sync_both(client, server)
+        sync_both(self.client, self.server)
 
         self.assertEqual(Document({'_id': 'A', 'value': 'val1', '_rev': 1}),
-                         client.get('A'))
+                         self.client.get('A'))
         self.assertEqual(Document({'_id': 'B', 'value': 'val2', '_rev': 1}),
-                         client.get('B'))
+                         self.client.get('B'))
 
         self.assertEqual(Document({'_id': 'A', 'value': 'val1', '_rev': 1}),
-                         server.get('A'))
+                         self.server.get('A'))
         self.assertEqual(Document({'_id': 'B', 'value': 'val2', '_rev': 1}),
-                         server.get('B'))
+                         self.server.get('B'))
 
     def test_overlapping_sync(self):
         """Overlapping documents from datastore"""
-        server = MemoryDatastore('server')
-        client = MemoryDatastore('client')
-
         # server makes object A v1
-        server.put(Document({'_id': 'A', 'value': 'val1'}))
-        server.put(Document({'_id': 'C', 'value': 'val3'}))
+        self.server.put(Document({'_id': 'A', 'value': 'val1'}))
+        self.server.put(Document({'_id': 'C', 'value': 'val3'}))
         # client makes object B v1
-        client.put(Document({'_id': 'B', 'value': 'val2'}))
-        client.put(Document({'_id': 'C', 'value': 'val4'}))
+        self.client.put(Document({'_id': 'B', 'value': 'val2'}))
+        self.client.put(Document({'_id': 'C', 'value': 'val4'}))
 
         # sync leaves both server and client with A val1,  B val2, C val4
-        sync_both(client, server)
+        sync_both(self.client, self.server)
 
         self.assertEqual(Document({'_id': 'A', 'value': 'val1', '_rev': 1}),
-                         client.get('A'))
+                         self.client.get('A'))
         self.assertEqual(Document({'_id': 'B', 'value': 'val2', '_rev': 1}),
-                         client.get('B'))
+                         self.client.get('B'))
         self.assertEqual(Document({'_id': 'C', 'value': 'val4', '_rev': 2}),
-                         client.get('C'))
+                         self.client.get('C'))
 
         self.assertEqual(Document({'_id': 'A', 'value': 'val1', '_rev': 1}),
-                         server.get('A'))
+                         self.server.get('A'))
         self.assertEqual(Document({'_id': 'B', 'value': 'val2', '_rev': 1}),
-                         server.get('B'))
+                         self.server.get('B'))
         self.assertEqual(Document({'_id': 'C', 'value': 'val4', '_rev': 2}),
-                         server.get('C'))
+                         self.server.get('C'))
 
     def test_long_streaks(self):
-        server = MemoryDatastore('server')
-        client = MemoryDatastore('client')
-
         items = ['a', 'b', 'c', 'd', 'e']
 
         for jdx in range(10):
@@ -70,49 +73,166 @@ class TestDatastore(unittest.TestCase):
                 # pick item
                 item = random.choice(items)
                 if random.uniform(0, 1) < 0.3:
-                    server.delete(item)
+                    self.server.delete(item)
                 else:
                     val = random.randint(0, 1000)
-                    server.put(Document({'_id': item, 'value': val}))
+                    self.server.put(Document({'_id': item, 'value': val}))
 
             for idx in range(30):
                 item = random.choice(items)
                 if random.uniform(0, 1) < 0.3:
-                    server.delete(item)
+                    self.server.delete(item)
                 else:
                     val = random.randint(0, 1000)
-                    server.put(Document({'_id': item, 'value': val}))
+                    self.server.put(Document({'_id': item, 'value': val}))
 
             # sync
-            sync_both(client, server)
+            sync_both(self.client, self.server)
 
             # server and client should now contain the same stuff
-            docs_c = [doc for doc in client.get_docs_since(0)]
-            docs_s = [doc for doc in server.get_docs_since(0)]
+            docs_c = [doc for doc in self.client.get_docs_since(0)]
+            docs_s = [doc for doc in self.server.get_docs_since(0)]
 
             self.assertEqual(sorted(docs_c), sorted(docs_s))
 
     def test_copy(self):
-        server = MemoryDatastore('server')
         doc = Document({'_id': 'A', 'value': 'val1'})
-        server.put(doc)
+        self.server.put(doc)
         doc['another'] = 'foo'
-        doc2 = server.get('A')
+        doc2 = self.server.get('A')
         self.assertTrue('another' not in doc2)
         self.assertTrue('another' in doc)
 
     def test_delete(self):
-        server = MemoryDatastore('server')
         doc = Document({'_id': 'A', 'value': 'val1'})
-        server.put(doc)
-        doc1 = server.get('A')
-        self.assertTrue(server.get('A'))
-        server.delete('A')
-        doc2 = server.get('A')
+        self.server.put(doc)
+        doc1 = self.server.get('A')
+        self.assertTrue(self.server.get('A'))
+        self.server.delete('A')
+        doc2 = self.server.get('A')
         import logging
         logging.info("doc2 %s" % doc2)
         self.assertEqual(True, doc2['_deleted'])
         self.assertGreater(doc2['_rev'], doc1['_rev'])
+
+
+class TestMemoryDatastore(_TestDatastore):
+    def setUp(self):
+        super().setUp()
+        self.server = MemoryDatastore('server')
+        self.client = MemoryDatastore('client')
+
+
+class TestPostgresDatastore(_TestDatastore):
+    client_connstr = None
+    server_connstr = None
+
+    @staticmethod
+    def _dbconnstr(dbname=None):
+        """Connect to test database on host db, user postgres."""
+        the_str = "host=db user=postgres"
+        if dbname:
+            the_str += ' dbname=%s' % dbname
+        return the_str
+
+    @staticmethod
+    def exec_sql(exec_func, dbname=None, autocommit=True):
+        """Execute some SQL.
+
+        Sometimes we want it with dbname, sometimes without.
+        Otherwise we'd just keep a cursor around instead.
+        """
+        connstr = TestPostgresDatastore._dbconnstr(dbname)
+        with psycopg2.connect(connstr) as conn:
+            if autocommit:
+                # Autocommit to be able to create a database
+                conn.set_isolation_level(
+                    psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+            with conn.cursor() as cursor:
+                exec_func(cursor)
+
+    @staticmethod
+    def _create_test_db(dbname):
+        TestPostgresDatastore._create_test_db1(dbname)
+        TestPostgresDatastore._create_test_db2(dbname)
+
+    @staticmethod
+    def _create_test_db1(dbname):
+        def exec_func(curs):
+            curs.execute(
+                "SELECT datname FROM pg_catalog.pg_database WHERE datname = %s",
+                (dbname,))
+            if not curs.fetchone():
+                curs.execute("CREATE DATABASE %s" % dbname)
+            else:
+                logger.info("database %s already exists" % dbname)
+        TestPostgresDatastore.exec_sql(exec_func, autocommit=True)
+
+    @staticmethod
+    def _create_table_if_not_exists(dbname: str, tablename: str,
+                                    definition: str):
+        def exec_func(curs):
+            # check for table existence
+            curs.execute(
+                "select * from information_schema.tables where table_name=%s",
+                (tablename,))
+            if not bool(curs.fetchone()):
+                # table doesn't exist, create it
+                curs.execute("CREATE TABLE %s (%s)" % (tablename, definition))
+        TestPostgresDatastore.exec_sql(exec_func, dbname=dbname)
+
+    @staticmethod
+    def _create_test_db2(dbname):
+        def exec_func(curs):
+            TestPostgresDatastore._create_table_if_not_exists(
+                dbname,
+                'data_sync_revisions',
+                'sequence_id int not null')
+            TestPostgresDatastore._create_table_if_not_exists(
+                dbname,
+                'docs',
+                '_id text UNIQUE not null, _rev int not null,'
+                ' value text, _deleted bool')
+            # Init sequence_id if not present
+            # "ON CONFLICT" requires postgres 9.5+
+            # See also https://stackoverflow.com/a/17267423/34935
+            # See also https://stackoverflow.com/a/30118648/34935
+            curs.execute(
+                "INSERT INTO data_sync_revisions (sequence_id) VALUES (0)"
+                " ON CONFLICT DO NOTHING")
+        TestPostgresDatastore.exec_sql(exec_func, dbname=dbname)
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestPostgresDatastore, cls).setUpClass()
+        # create server and client databases
+        cls.server_connstr = TestPostgresDatastore._dbconnstr('test_server')
+        TestPostgresDatastore._create_test_db('test_server')
+        cls.client_connstr = TestPostgresDatastore._dbconnstr('test_client')
+        TestPostgresDatastore._create_test_db('test_client')
+
+    @staticmethod
+    def _drop_db(dbname):
+        TestPostgresDatastore.exec_sql(
+            lambda curs: curs.execute("DROP DATABASE %s" % dbname))
+
+    @classmethod
+    def tearDownClass(cls):
+        TestPostgresDatastore._drop_db('test_server')
+        TestPostgresDatastore._drop_db('test_client')
+
+    def setUp(self):
+        super().setUp()
+        self.server = PostgresDatastore(
+            'server', TestPostgresDatastore.server_connstr, 'docs')
+        self.server.__enter__()
+        self.client = PostgresDatastore(
+            'client', TestPostgresDatastore.client_connstr, 'docs')
+        self.client.__enter__()
+
+    def tearDown(self) -> None:
+        self.server.__exit__()
+        self.client.__exit__()
 
 
 class TestDocument(unittest.TestCase):
@@ -122,3 +242,12 @@ class TestDocument(unittest.TestCase):
         doc2 = Document({'_id': 'A', 'value': 'val2'})
         self.assertGreater(doc2, doc)
         self.assertLess(doc, doc2)
+
+    def test_none(self):
+        doc1 = Document({'_id': 'A', 'value': 'val1'})
+        doc2 = Document({'_id': 'A', 'value': None})
+        # equality with None
+        self.assertEqual(doc2, doc2)
+        # inequality with None
+        self.assertGreater(doc1, doc2)
+        self.assertLess(doc2, doc1)
