b11c6c8181a8ba3b2dd01f9961365673ed9ca0d7
Dan Frankowski <dfrankow@gmail.com>
2020-12-26 16:23:40 -0600

    Add required packages to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
+import os
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+def read_requirements():
+    current = os.path.dirname(os.path.realpath(__file__))
+    requirement_path = current + '/requirements.txt'
+    install_requires = []
+    if os.path.isfile(requirement_path):
+        with open(requirement_path) as f:
+            install_requires = f.read().splitlines()
+    return install_requires
+
 
 setuptools.setup(
     name="reldatasync",
@@ -9,6 +20,7 @@ setuptools.setup(
     author="Dan Frankowski",
     author_email="dfrankow+rds@gmail.com",
     description="Synchronize relational data between two entities",
+    install_requires=read_requirements(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dfrankow/data_sync",
