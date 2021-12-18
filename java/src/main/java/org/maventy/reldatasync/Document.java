50a70357dddc431c27f25acaff142cb8725ba594
Dan Frankowski <dfrankow@gmail.com>
2021-12-18 17:49:26 -0600

    Add fromDocumentsJson

--- java/src/main/java/org/maventy/reldatasync/Document.java
+++ java/src/main/java/org/maventy/reldatasync/Document.java
@@ -1,15 +1,26 @@
 package org.maventy.reldatasync;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Document extends TreeMap<String, String> {
     private static final String ID = "_id";
+
+    public static List<Document> fromDocumentsJson(JSONArray ja) throws ParseException {
+        List<Document> docs = new ArrayList<Document>();
+        for (Object obj : ja) {
+            Map.Entry entry = (Map.Entry) obj;
+            docs.add(new Document((JSONObject) obj));
+        }
+        return docs;
+    }
 
     public Document(String json) throws ParseException {
         // Parse json from the string and put it in the map
