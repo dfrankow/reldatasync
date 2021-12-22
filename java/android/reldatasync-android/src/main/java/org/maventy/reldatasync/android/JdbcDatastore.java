d4e98ab68c7057f2e61287ba99629a617d8f15cd
Dan Frankowski <dfrankow@gmail.com>
2021-12-22 11:19:50 -0600

    Write the beginning of get

--- java/android/reldatasync-android/src/main/java/org/maventy/reldatasync/android/JdbcDatastore.java
+++ java/android/reldatasync-android/src/main/java/org/maventy/reldatasync/android/JdbcDatastore.java
@@ -1,11 +1,15 @@
 package org.maventy.reldatasync.android;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +17,71 @@ import org.maventy.reldatasync.Document;
 
 // Communicate to a REST server.
 public class JdbcDatastore {
+    private final Connection conn;
     private final String table;
 
-    public JdbcDatastore(String table) {
+    public JdbcDatastore(Connection conn, String table) {
+        this.conn = conn;
         this.table = table;
     }
 
-    public Document get(String docid) {
-        return null;
+    private static class NamesAndTypes {
+        public final List<String> names;
+        public final List<Integer> types;
+        public NamesAndTypes(List<String> names, List<Integer> types) {
+            this.names = names;
+            this.types = types;
+        }
+    };
+
+    private static NamesAndTypes getColumnNamesAndTypes(ResultSet rs) throws SQLException {
+        List<String> names = new ArrayList<>();
+        List<Integer> types = new ArrayList<>();
+        ResultSetMetaData meta = rs.getMetaData();
+        int count = meta.getColumnCount();
+        for(int idx = 1; idx <=count; idx++) {
+            String name = meta.getColumnName(idx);
+            names.add(name);
+            types.add(meta.getColumnType(idx));
+        }
+        return new NamesAndTypes(names, types);
+    }
+
+    private static List<String> getColumnNames(ResultSet rs) throws SQLException {
+        List<String> names = new ArrayList<>();
+        ResultSetMetaData meta = rs.getMetaData();
+        int count = meta.getColumnCount();
+        for(int idx = 1; idx <=count; idx++) {
+            String name = meta.getColumnName(idx);
+            names.add(name);
+        }
+        return names;
+    }
+
+    public Document get(String docid) throws SQLException {
+        Document doc = null;
+        String sql = "SELECT * FROM " + table + " WHERE " + Document.ID + " = ?";
+        try (PreparedStatement getSt = conn.prepareStatement(sql)) {
+            getSt.setString(1, docid);
+            ResultSet rs = getSt.executeQuery();
+            if (rs.next()) {
+                JSONObject jo = new JSONObject();
+
+                NamesAndTypes nat = getColumnNamesAndTypes(rs);
+                for (int idx = 0; idx < nat.names.size(); idx++) {
+                    int type = nat.types.get(idx);
+                    // Check if type is supported
+                    if (!(type == Types.INTEGER || type == Types.CHAR)) {
+                        throw new SQLException("Unsupported type " + type);
+                    }
+
+                    String name = nat.names.get(idx);
+                    jo.put(name, rs.getObject(idx));
+                }
+                doc = new Document(jo);
+            }
+        }
+        return doc;
     }
 
     public void put(final Document doc) throws IOException {
--- java/android/reldatasync-android/src/test/java/org/maventy/reldatasync/android/TestJdbcDataStore.java
+++ java/android/reldatasync-android/src/test/java/org/maventy/reldatasync/android/TestJdbcDataStore.java
@@ -2,6 +2,7 @@ package org.maventy.reldatasync.android;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.maventy.reldatasync.Document;
 import org.robolectric.RobolectricTestRunner;
 
 import java.io.File;
@@ -10,12 +11,15 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(RobolectricTestRunner.class)
 public class TestJdbcDataStore {
-    private Connection connection = null;
+    private Connection conn = null;
 
     private void setConnection(File file) throws SQLException {
         try {
@@ -24,14 +28,34 @@ public class TestJdbcDataStore {
             throw new RuntimeException("Failed to register SQLDroidDriver");
         }
         String jdbcUrl = "jdbc:sqldroid:" + file.getAbsolutePath();
-        this.connection = DriverManager.getConnection(jdbcUrl);
+        this.conn = DriverManager.getConnection(jdbcUrl);
     }
 
+    private void createTable1() throws SQLException {
+        String sql = "CREATE TABLE table1 " +
+                "("+ Document.ID + " VARCHAR(32) not NULL, " +
+                " first VARCHAR(255), " +
+                " last VARCHAR(255), " +
+                " age INTEGER, " +
+                " PRIMARY KEY ( " + Document.ID + " ))";
+
+        try (Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(sql);
+        }
+    }
     @Test
     public void test1() throws IOException, SQLException {
         try (AutoDeletingTempFile file = new AutoDeletingTempFile("tmp", ".db");) {
             setConnection(file.getFile());
             assertEquals(1, 1);
         }
+
+        // Table created outside JdbcDatastore
+        createTable1();
+        JdbcDatastore jds = new JdbcDatastore(conn, "table1");
+        UUID id = UUID.randomUUID();
+        // remove -s
+        String foo = id.toString().replaceAll("-", "");
+        assertNull(jds.get(foo));
     }
 }
