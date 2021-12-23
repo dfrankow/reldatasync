043848f7659fd0d48ee0626be08f4ed4d9dcb32e
Dan Frankowski <dfrankow@gmail.com>
2021-12-23 14:31:01 -0600

    Add Document.compareTo

--- java/android/reldatasync/src/main/java/org/maventy/reldatasync/Document.java
+++ java/android/reldatasync/src/main/java/org/maventy/reldatasync/Document.java
@@ -6,11 +6,14 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Document extends TreeMap<String, Object> {
+public class Document extends TreeMap<String, Object> implements Comparable<Document> {
     public static final String ID = "_id";
     public static final String REV = "_rev";
     public static final String DELETED = "_deleted";
@@ -69,25 +72,88 @@ public class Document extends TreeMap<String, Object> {
             return false;
         Document doc = (Document) obj;
 
-        // All keys must be the same
-        if (!keySet().equals(doc.keySet()))
-            return false;
-
-        // All values must be the same (using equals)
-        for (String key: keySet()) {
-            Object val1 = get(key);
-            Object val2 = doc.get(key);
-            if (val1 == null && val2 != null)
-                return false;
-            if (val1 != null && !val1.equals(val2))
-                return false;
-        }
-
-        return true;
+        return this.compareTo(doc) == 0;
     }
 
     @Override
     public Document clone() {
         return new Document(this);
+    }
+
+    private int compareVals(Object val1, Object val2) {
+        if (val1 == null && val2 == null)
+            return 0;
+        if (val1 == null && val2 != null)
+            return -1;
+        if (val1 != null && val2 == null)
+            return 1;
+        assert val1 != null && val2 != null;
+
+        int ret = val1.getClass().getName().compareTo(val2.getClass().getName());
+        if (ret != 0) {
+            throw new IllegalArgumentException("classes are not the same");
+        }
+
+        // We want only comparable types here
+        Class clazz = val1.getClass();
+
+        if (!Comparable.class.isAssignableFrom(clazz)) {
+            throw new IllegalArgumentException("classes are not Comparable");
+        }
+
+        // It's comparable, so compare
+        Comparable<Object> c1 = (Comparable<Object>) val1;
+        Comparable<Object> c2 = (Comparable<Object>) val2;
+        return (c1.compareTo(c2));
+    }
+
+    @Override
+    public int compareTo(Document doc) {
+        // NOTE: This should return the same result as the python version,
+        // otherwise there are corner cases that would synchronize differently.
+        // This probably requires a LOT of test coverage to get right.
+        // Example: same number of keys but different key names.
+        // Example: checking every type of object.
+
+        // self check
+        if (this == doc)
+            return 0;
+
+        // null check
+        if (doc == null)
+            return 1;
+
+        if (keySet().size() < doc.keySet().size())
+            return -1;
+        if (keySet().size() > doc.keySet().size())
+            return 1;
+
+        // same number of keys, now compare them
+        Comparator<Object> comp = new Comparator<Object>() {
+            @Override
+            public int compare(Object obj, Object obj2) {
+                return compareVals(obj, obj2);
+            }
+        };
+        List<Object> keys = Arrays.asList(keySet().toArray());
+        Collections.sort(keys, comp);
+        List<Object> docKeys = Arrays.asList(doc.keySet().toArray());
+        Collections.sort(docKeys, comp);
+        for (int idx = 0; idx < keys.size(); idx++) {
+            int keycomp = compareVals(keys.get(idx), docKeys.get(idx));
+            if (keycomp != 0)
+                return keycomp;
+        }
+
+        // All values must be the same (using equals)
+        for (String key: keySet()) {
+            Object val1 = get(key);
+            Object val2 = doc.get(key);
+            int ret = compareVals(val1, val2);
+            if (ret != 0)
+                return ret;
+        }
+
+        return 0;
     }
 }
