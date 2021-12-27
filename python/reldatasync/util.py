ac5791fa1ca94986c8d3240d3a5581dc6ec423bc
Dan Frankowski <dfrankow@gmail.com>
2021-12-26 18:35:45 -0600

    standard basic_config for logging

--- python/reldatasync/util.py
+++ python/reldatasync/util.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import logging
 
 
 def dict_hash(a_dict):
@@ -8,3 +9,10 @@ def dict_hash(a_dict):
     See https://stackoverflow.com/a/22003440."""
     dict_str = json.dumps(a_dict, sort_keys=True, default=str)
     return hashlib.md5(dict_str.encode('utf8')).hexdigest()
+
+
+def basic_config(level):
+    """basicConfig with a standard logging format"""
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s")
