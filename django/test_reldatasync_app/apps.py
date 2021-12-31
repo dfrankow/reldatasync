481ded0bdee93f73cdc83c55833777963b71fd0e
Dan Frankowski <dfrankow@gmail.com>
2021-12-31 14:49:18 -0600

    tweak

--- django/test_reldatasync_app/apps.py
+++ django/test_reldatasync_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestRsdbAppConfig(AppConfig):
+class TestReldatasyncAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'test_reldatasync_app'
